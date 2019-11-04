3089056b39c25036cc81a02d2225fc8f194a1d83
PBentley <perri.bentley@gmail.com>
2019-11-04 10:50:44 -0600

    Final Game
    Completed Game

---
+++ Assets/scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public string NewGameScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(NewGameScene);
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Survivng-Suburbia
+ 
