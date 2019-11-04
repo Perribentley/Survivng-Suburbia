d551c004dc6de2cba4298e8fffa50f2e44acff4b
PBentley <perri.bentley@gmail.com>
2019-11-04 15:11:36 -0600

    Add files via upload
    a bunch of shit from class idk if this will work

---
+++ scripts/MouseLook.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField] float VerticalMouseSens = 0.5f;
+    [SerializeField] float HorizontalMouseSens = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get X and Y axis
+        float MouseHorizontal = Input.GetAxis("Mouse X") * HorizontalMouseSens;
+        float MouseVertical = Input.GetAxis("Mouse Y") * VerticalMouseSens;
+
+        //Rotate Camera
+        float CameraRotX = transform.eulerAngles.x + MouseHorizontal;
+        float CameraRotY = transform.eulerAngles.y + MouseVertical;
+
+        transform.eulerAngles = new Vector3(CameraRotX, CameraRotY, 0);
+    }
+}
---
+++ scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] float Speed = 10f;
+    [SerializeField] float TurnSpeed = 20f;
+    private float HorizontalInput;
+    private float ForwardInput;
+
+    private Rigidbody RBPlayer;
+
+    private int FuseCount;
+    [SerializeField] Text Fuses;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get player input
+        HorizontalInput = Input.GetAxis("Horizontal");
+        ForwardInput = Input.GetAxis("Vertical");
+
+        //Move player forward
+        transform.Translate(Vector3.forward * Time.deltaTime * Speed * ForwardInput);
+        //Turn player
+        transform.Rotate(Vector3.up, Time.deltaTime * TurnSpeed * HorizontalInput);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        RBPlayer = GetComponent<Rigidbody>();
+
+        if(collision.gameObject.name == "House")
+        {
+            RBPlayer.velocity = Vector3.zero;
+        }
+
+        if(collision.gameObject.name == "Street")
+        {
+            RBPlayer.velocity = Vector3.zero;
+        }
+
+        if(collision.gameObject.name == "Walls")
+        {
+            RBPlayer.velocity = Vector3.zero;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Fuse")
+        {
+            FuseCount += 1;
+            Debug.Log(FuseCount);
+            SetFuseText();
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    void SetFuseText()
+    {
+        Fuses.text = "Fuses: " + FuseCount.ToString();
+    }
+}
